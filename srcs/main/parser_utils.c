9b3f2831656f9662e4030674850f6e755bab4a52
Leo Rapini <leorapini@gmail.com>
2021-06-03 17:06:55 -0300

    error management and tests for parser

--- srcs/main/parser_utils.c
+++ srcs/main/parser_utils.c
@@ -6,7 +6,7 @@
 /*   By: lpinheir <lpinheir@student.42sp.org.b      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/27 15:14:31 by lpinheir          #+#    #+#             */
-/*   Updated: 2021/06/03 10:23:15 by lpinheir         ###   ########.fr       */
+/*   Updated: 2021/06/03 16:57:14 by lpinheir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,29 @@ static char	*join_hex(char *hex_dest, char *hex)
 	return (hex_dest);
 }
 
+/* Auxiliary function to rgb_to_int. Receives string and checks if value
+is a number. Exits with error if that's not the case. Returns hex value
+as a string */
+static char	*check_hex(char *number)
+{
+	char	*hex;
+	int		int_num;
+
+	int_num = 0;
+	if (!(ft_strisdigit(number)))
+		error("F or C values aren't numbers");
+	if (!(ft_strncmp(number, "0", 1)))
+		hex = ft_strdup("00");
+	else
+	{
+		int_num = ft_atoi(number);
+		if (int_num > 255)
+			error("F or C values are bigger than 255");
+		hex = ft_uitoh(int_num, 0);
+	}
+	return (hex);
+}
+
 /* Receives a rgb string in the format Ex. 100,100,0 and returns the int
 value referent to that rgb value */
 int	rgb_to_int(char	*rgb_color)
@@ -53,20 +76,20 @@ int	rgb_to_int(char	*rgb_color)
 	total_hex = ft_strdup("");
 	while (numbers[i] != NULL)
 	{
-		if (!(ft_strncmp(numbers[i], "0", 1)))
-			hex = ft_strdup("00");
-		else
-			hex = ft_uitoh(ft_atoi(numbers[i]), 0);
+		hex = check_hex(numbers[i]);
 		total_hex = join_hex(total_hex, hex);
 		free(hex);
 		i++;
 	}
+	if (i != 3)
+		error("Bad F or C color value");
 	results = ft_htoui(total_hex);
 	free(total_hex);
 	free_split(numbers);
 	return (results);
 }
 
+/* Receives a file location and checks if file exists  */
 int	check_file_exists(char const *file)
 {
 	int	fd;
@@ -75,7 +98,7 @@ int	check_file_exists(char const *file)
 	if (fd < 1)
 	{
 		printf("Problem with file %s\n", file);
-		error("File Doesn't Exist");
+		error("File Not Found");
 	}
 	close(fd);
 	return (0);
--- test_parser.sh
+++ test_parser.sh
@@ -10,7 +10,7 @@ echo "\nTesting r_wrong_format.cub"
 ./cub3d ./tests/parser/r_wrong_format.cub
 
 echo "\nTesting r_wrong_format2.cub"
-./cub3d ./tests/parser/r_wrong_format.cub
+./cub3d ./tests/parser/r_wrong_format2.cub
 
 echo "\nTesting r_empty_value.cub"
 ./cub3d ./tests/parser/r_empty_value.cub
@@ -34,4 +34,109 @@ echo "\nTesting no_file_not_found.cub"
 
 echo "\nTesting no_file_not_found_2.cub"
 ./cub3d ./tests/parser/no_file_not_found_2.cub
+echo ""
+
+echo "\n++++++++++++++ TEST SO VALUES +++++++++++++++"
+echo "Testing success.cub"
+./cub3d ./tests/parser/success.cub
+
+echo "\nTesting so_empty_value.cub"
+./cub3d ./tests/parser/so_empty_value.cub
+
+echo "\nTesting so_no_so.cub"
+./cub3d ./tests/parser/so_no_so.cub
+
+echo "\nTesting so_file_not_found.cub"
+./cub3d ./tests/parser/so_file_not_found.cub
+
+echo "\nTesting so_file_not_found_2.cub"
+./cub3d ./tests/parser/so_file_not_found_2.cub
+echo ""
+
+echo "\n++++++++++++++ TEST WE VALUES +++++++++++++++"
+echo "Testing success.cub"
+./cub3d ./tests/parser/success.cub
+
+echo "\nTesting we_empty_value.cub"
+./cub3d ./tests/parser/we_empty_value.cub
+
+echo "\nTesting we_no_so.cub"
+./cub3d ./tests/parser/we_no_we.cub
+
+echo "\nTesting we_file_not_found.cub"
+./cub3d ./tests/parser/we_file_not_found.cub
+
+echo "\nTesting we_file_not_found_2.cub"
+./cub3d ./tests/parser/we_file_not_found_2.cub
+echo ""
+
+echo "\n++++++++++++++ TEST EA VALUES +++++++++++++++"
+echo "Testing success.cub"
+./cub3d ./tests/parser/success.cub
+
+echo "\nTesting ea_empty_value.cub"
+./cub3d ./tests/parser/ea_empty_value.cub
+
+echo "\nTesting ea_no_ea.cub"
+./cub3d ./tests/parser/ea_no_ea.cub
+
+echo "\nTesting ea_file_not_found.cub"
+./cub3d ./tests/parser/ea_file_not_found.cub
+
+echo "\nTesting ea_file_not_found_2.cub"
+./cub3d ./tests/parser/ea_file_not_found_2.cub
+echo ""
+
+echo "\n++++++++++++++ TEST S VALUES +++++++++++++++"
+echo "Testing success.cub"
+./cub3d ./tests/parser/success.cub
+
+echo "\nTesting spr_empty_value.cub"
+./cub3d ./tests/parser/spr_empty_value.cub
+
+echo "\nTesting spr_no_spr.cub"
+./cub3d ./tests/parser/spr_no_spr.cub
+
+echo "\nTesting spr_file_not_found.cub"
+./cub3d ./tests/parser/spr_file_not_found.cub
+echo ""
+
+echo "\n++++++++++++++ TEST F VALUES +++++++++++++++"
+echo "Testing success.cub"
+./cub3d ./tests/parser/success.cub
+
+echo "\nTesting f_wrong_format.cub"
+./cub3d ./tests/parser/f_wrong_format.cub
+
+echo "\nTesting f_wrong_format_2.cub"
+./cub3d ./tests/parser/f_wrong_format_2.cub
+
+echo "\nTesting f_wrong_format_3.cub"
+./cub3d ./tests/parser/f_wrong_format_3.cub
+
+echo "\nTesting f_empty_value.cub"
+./cub3d ./tests/parser/f_empty_value.cub
+
+echo "\nTesting f_no_f.cub"
+./cub3d ./tests/parser/f_no_f.cub
+echo ""
+
+echo "\n++++++++++++++ TEST C VALUES +++++++++++++++"
+echo "Testing success.cub"
+./cub3d ./tests/parser/success.cub
+
+echo "\nTesting c_wrong_format.cub"
+./cub3d ./tests/parser/c_wrong_format.cub
+
+echo "\nTesting c_wrong_format_2.cub"
+./cub3d ./tests/parser/c_wrong_format_2.cub
+
+echo "\nTesting c_wrong_format_3.cub"
+./cub3d ./tests/parser/c_wrong_format_3.cub
+
+echo "\nTesting c_empty_value.cub"
+./cub3d ./tests/parser/c_empty_value.cub
+
+echo "\nTesting c_no_c.cub"
+./cub3d ./tests/parser/c_no_c.cub
 echo ""
